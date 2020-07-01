c1958d2cde7bf4ecb98dbfd8fa1c3699a7e3008b
Gurkirat Khaira <777gurkirat@gmail.com>
2020-07-01 13:12:48 -0400

    Added tiktokmanager class with completed constructor

--- Assignment2/TiktokManager.cs
+++ Assignment2/TiktokManager.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Linq.Expressions;
 
 namespace Assignment2
 {
-    class TiktokManager
+    public static class TiktokManager
     {
+        private static List<TikTok> TIKTOKS;
+        private static string FILENAME = "tiktok.txt";
+
+        static TiktokManager()
+        {
+            TIKTOKS = new List<TikTok>();
+            string line;
+            StreamReader file = new StreamReader(FILENAME);
+            while (null != (line = file.ReadLine()))
+            {
+                line = file.ReadLine();
+                Console.WriteLine(line);
+                TIKTOKS.Add(TikTok.Parse(line));
+            }
+        }
+        public static void Initialize()
+        {
+
+        }
+        public static void Show()
+        {
+
+        }
+        public static void Show(string hashtag)
+        {
+
+        }
+        public static void Show(int length)
+        {
+
+        }
+        public static void Show(Audience audience)
+        {
+
+        }
+
     }
 }
