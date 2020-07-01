f4062db4d99b040e988a04376963fa60faa2a2bc
Gurkirat Khaira <777gurkirat@gmail.com>
2020-07-01 13:11:34 -0400

    Add project files.

---
+++ Assignment2/TikTok.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment2
+{
+    enum Audience { World, Group, Special }
+    class TikTok
+    {
+        private static int _ID;
+        public string Originator { get; }
+        public int Length { get; }
+        public string HashTag { get; }
+        public Audience Audience { get; }
+        public int Id { get; }
+
+        public TikTok(string originator, int length, string hashTag, Audience audience)
+        {
+            Originator = originator;
+            Length = length;
+            HashTag = hashTag;
+            Audience = audience;
+            Id = _ID;
+            _ID++;
+        }
+
+        public TikTok(int id, string originator, int length, string hashTag, Audience audience)
+        {
+            Id = id;
+            Originator = originator;
+            Length = length;
+            HashTag = hashTag;
+            Audience = audience;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static TikTok Parse(string line)
+        {
+
+            String[] parts = line.Split('\t');
+            string origin = parts[0];
+            int len = Convert.ToInt32(parts[1]);
+            string hash = parts[2];
+            Audience aud = (Audience)Enum.Parse(typeof(Audience), parts[3]);
+            TikTok t = new TikTok(01, origin, len, hash, aud);
+            return t;
+        }
+    }
+}
